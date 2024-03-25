1d93e7bcfe6f509e073c9c45333f8172ba1863f3
Srayashib <150220175+Srayashib@users.noreply.github.com>
2024-03-25 21:31:26 +0530

    Add files via upload

---
+++ front end/script.js
@@ -0,0 +1,85 @@
+
+document.getElementById('blockA').addEventListener('mouseover', function() {
+    document.getElementById('infoBox').style.display = 'block';
+});
+
+document.getElementById('blockA').addEventListener('mouseout', function() {
+    document.getElementById('infoBox').style.display = 'none';
+});
+document.getElementById('blockB').addEventListener('mouseover', function() {
+    document.getElementById('infoBox').style.display = 'block';
+});
+
+document.getElementById('blockB').addEventListener('mouseout', function() {
+    document.getElementById('infoBox').style.display = 'none';
+});
+document.getElementById('blockC').addEventListener('mouseover', function() {
+    document.getElementById('infoBox').style.display = 'block';
+});
+
+document.getElementById('blockC').addEventListener('mouseout', function() {
+    document.getElementById('infoBox').style.display = 'none';
+});
+document.getElementById('blockD').addEventListener('mouseover', function() {
+    document.getElementById('infoBox').style.display = 'block';
+});
+
+document.getElementById('blockD').addEventListener('mouseout', function() {
+    document.getElementById('infoBox').style.display = 'none';
+});
+document.getElementById('bk1').addEventListener('mouseover', function() {
+  document.getElementById('infoBox1').style.display = 'block';
+});
+
+document.getElementById('bk1').addEventListener('mouseout', function() {
+  document.getElementById('infoBox1').style.display = 'none';
+});
+document.getElementById('bk2').addEventListener('mouseover', function() {
+  document.getElementById('infoBox2').style.display = 'block';
+});
+
+document.getElementById('bk2').addEventListener('mouseout', function() {
+  document.getElementById('infoBox2').style.display = 'none';
+});
+document.getElementById('bk3').addEventListener('mouseover', function() {
+  document.getElementById('infoBox3').style.display = 'block';
+});
+
+document.getElementById('bk3').addEventListener('mouseout', function() {
+  document.getElementById('infoBox3').style.display = 'none';
+});
+document.getElementById('bk4').addEventListener('mouseover', function() {
+  document.getElementById('infoBox4').style.display = 'block';
+});
+
+document.getElementById('bk4').addEventListener('mouseout', function() {
+  document.getElementById('infoBox4').style.display = 'none';
+});
+document.getElementById('bk5').addEventListener('mouseover', function() {
+  document.getElementById('infoBox5').style.display = 'block';
+});
+
+document.getElementById('bk5').addEventListener('mouseout', function() {
+  document.getElementById('infoBox5').style.display = 'none';
+});
+document.getElementById('bk6').addEventListener('mouseover', function() {
+  document.getElementById('infoBox6').style.display = 'block';
+});
+
+document.getElementById('bk6').addEventListener('mouseout', function() {
+  document.getElementById('infoBox6').style.display = 'none';
+});
+document.getElementById('bk7').addEventListener('mouseover', function() {
+  document.getElementById('infoBox7').style.display = 'block';
+});
+
+document.getElementById('bk7').addEventListener('mouseout', function() {
+  document.getElementById('infoBox7').style.display = 'none';
+});
+document.getElementById('bk8').addEventListener('mouseover', function() {
+  document.getElementById('infoBox8').style.display = 'block';
+});
+
+document.getElementById('bk8').addEventListener('mouseout', function() {
+  document.getElementById('infoBox8').style.display = 'none';
+});
